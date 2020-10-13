445997808d12bd76cda1bbd1ad29e99136a6f339
mdus <marcin.dus@nokia.com>
2020-10-13 15:35:27 +0200

    scoped enum added

--- shapes/Shape.hpp
+++ shapes/Shape.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+enum class Color : unsigned int
+    {
+        GREEN,PINK,YELLOW
+    };
+
+
 class Shape
 {
 public:
@@ -8,4 +14,7 @@ public:
     virtual double getArea() const = 0;
     virtual double getPerimeter() const = 0;
     virtual void print() const;
+
+private:
+    Color color = Color::PINK;
 };
